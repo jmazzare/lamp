b5240848d06a15a9b1dc235c9a84c8f0cc9269b4
Glitch (bustling-aragon) <none>
2019-10-04 00:22:54 +0000

    🚜🏦 Updated with Glitch

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
+<?php
+ini_set('display_errors', 1);
+
+  ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+?>
 <html>
   <head>
     <title>LAMP Proof of Concept</title>
   </head>
   <body>
+  <form action="./form-handler.php" method="GET">
+  <label>
+  choose your birth year
+  <select name="year">
+  <option value="">select year</option>
     <?php
-    phpinfo();
+      $year = date('Y');
+      for($i = 1950; $i <= $year; $i++) {
+        echo "<option>$i</option>";
+      }
     ?>
-  </body>
+    </select>
+  </label>
+  <br />
+  <br />
+      <input type="text" />
+    <input type="submit" value="Submit Form" />
+</form>
+
+        
+  
+        </body>
 </html>
