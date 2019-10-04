4e19fd799744870f3631a4b65268ff73bedfcc46
oh Kay <khtdr.com@gmail.com>
2019-10-03 18:32:05 -0600

    form handler

--- form-handler.php
+++ form-handler.php
@@ -1,2 +1,15 @@
 <?php
-  phpinfo();
+
+$year = @$_POST['birth-year'];
+$tele = @$_POST['telephone'];
+
+if (empty($year)) {
+  echo "You must provide a year.";
+}
+if (empty($telephone)) {
+  echo "You must provide a telephone number.";
+}
+
+if (!preg_match('/^[\d -\(\)]+$/', $telephone)) {
+  echo "Invalid phone number";
+}
--- index.php
+++ index.php
@@ -1,34 +1,28 @@
-<?php
-ini_set('display_errors', 1);
-
-  ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-?>
+<!doctype html>
 <html>
   <head>
-    <title>LAMP Proof of Concept</title>
+    <title>cs-401</title>
   </head>
   <body>
-  <form action="./form-handler.php" method="GET">
-  <label>
-  choose your birth year
-  <select name="year">
-  <option value="">select year</option>
-    <?php
-      $year = date('Y');
-      for($i = 1950; $i <= $year; $i++) {
-        echo "<option>$i</option>";
-      }
-    ?>
-    </select>
-  </label>
-  <br />
-  <br />
-      <input type="text" />
-    <input type="submit" value="Submit Form" />
-</form>
-
-        
-  
-        </body>
+    <form action="./form-handler.php" method="GET">
+      Simple Form
+      <label>
+        Choose your birth year
+        <select name="birth-year">
+          <option value="">select year</option>
+          <?php
+          $year = date('Y');
+          for($i = 1950; $i <= $year; $i++) {
+              echo "<option>$i</option>";
+          }
+          ?>
+        </select>
+      </label>
+      <label>
+        Enter your telephone number
+        <input type="text" name="telephone" />
+      </label>
+      <input type="submit" value="Submit Form" />
+    </form>
+  </body>
 </html>
